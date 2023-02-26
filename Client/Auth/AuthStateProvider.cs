bd7b62a59f762e9f1dc7e63474f78fd021c0f994
Tony Richards <trichards57@pm.me>
2023-02-26 18:26:00 +0000

    Added basic authentication.

---
+++ Client/Auth/AuthStateProvider.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Text.Json.Serialization;
+
+namespace BlazorApp.Client.Auth
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly IConfiguration config;
+
+        private readonly HttpClient http;
+
+        public AuthStateProvider(IConfiguration config, IWebAssemblyHostEnvironment environment)
+        {
+            this.config = config;
+            this.http = new HttpClient { BaseAddress = new Uri(environment.BaseAddress) };
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var authDataUrl = config.GetValue("StaticWebAppsAuthentication:AuthenticationDataUrl", "/.auth/me");
+                var data = await http.GetFromJsonAsync<ClientPrincipal>(authDataUrl);
+
+                var principal = data with { UserRoles = data.UserRoles.Except(new string[] { "anonymous" }, StringComparer.OrdinalIgnoreCase) };
+
+                if (!principal.UserRoles.Any())
+                    return new AuthenticationState(new ClaimsPrincipal());
+
+                var identity = new ClaimsIdentity(principal.IdentityProvider);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+                identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+                identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+                return new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+            catch
+            {
+                return new AuthenticationState(new ClaimsPrincipal());
+            }
+        }
+
+        private readonly record struct AuthenticationData
+        {
+            public ClientPrincipal ClientPrincipal { get; init; }
+        }
+
+        private readonly record struct ClientPrincipal
+        {
+            [JsonPropertyName("identityProvider")]
+            public string IdentityProvider { get; init; }
+
+            [JsonPropertyName("userDetails")]
+            public string UserDetails { get; init; }
+
+            [JsonPropertyName("userId")]
+            public string UserId { get; init; }
+
+            [JsonPropertyName("userRoles")]
+            public IEnumerable<string> UserRoles { get; init; }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using BlazorApp.Client.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) })
+    .AddAuthorizationCore()
+    .AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+
 
 await builder.Build().RunAsync();
