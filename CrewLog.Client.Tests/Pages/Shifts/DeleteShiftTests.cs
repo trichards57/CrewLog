618877907c49aba5f749f0ffa5445fb4910c7115
Tony Richards <trichards57@pm.me>
2023-03-09 05:18:44 +0000

    Started adding shift pages.

---
+++ CrewLog.Client.Tests/Pages/Shifts/DeleteShiftTests.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture;
+using BlazorStrap;
+using Bunit;
+using CrewLog.Client.Pages.Shifts;
+using CrewLog.Client.Stores;
+using CrewLog.Shared.Model;
+using FluentAssertions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrewLog.Client.Tests.Pages.Shifts
+{
+    public class DeleteShiftTests
+    {
+        private readonly Guid TestId = new("72965CE9-4793-4B48-B27F-603B7732F583");
+        private readonly Fixture Fixture = new Fixture();
+
+        [Fact]
+        public void RendersSuccessfully()
+        {
+            using var context = new TestContext();
+
+            var testShift = Fixture.Build<Shift>()
+                .With(s => s.Id, TestId)
+                .With(s => s.Date, new DateOnly(2022, 10, 11))
+                .With(s => s.StartTime, new TimeOnly(10, 00))
+                .With(s => s.EndTime, new TimeOnly(19, 00))
+                .Create();
+
+            var mockStore = new Mock<IStore<Shift>>();
+            mockStore.Setup(s => s.IsLoading).Returns(false);
+            mockStore.Setup(s => s.IsLoadingItem(It.IsAny<Guid>())).Returns(false);
+            mockStore.Setup(s => s.Data).Returns(new Dictionary<Guid, Shift> { { TestId, testShift } });
+
+            var mockNavigation = new Mock<NavigationManager>();
+
+            context.Services.AddSingleton(mockNavigation.Object);
+            context.Services.AddSingleton(mockStore.Object);
+            context.Services.AddBlazorStrap();
+
+            var component = context.RenderComponent<DeleteShift>(p =>
+                p.Add(c => c.Id, TestId)
+            );
+
+            component.Find("dd").TextContent.Should().Be(testShift.Name);
+            mockStore.Verify(s => s.LoadItems(new[] { TestId }, false, default));
+        }
+    }
+}
---
+++ CrewLog.Client.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CrewLog.Client.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
